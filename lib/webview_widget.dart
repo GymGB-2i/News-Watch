e263401c3dba160afc0684f4aab7dff6de5bccf7
Jonathan <bittner.jf@gmail.com>
2021-11-11 08:57:03 +0100

    add webview widget

---
+++ lib/webview_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class webview_widget extends StatelessWidget {
+  const webview_widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
