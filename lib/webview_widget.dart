ba010a11b14c331fb214ed5a49e47c1b7a5019e7
Jonathan <bittner.jf@gmail.com>
2021-11-11 09:15:16 +0100

    created webview_widget / converted it to a stateful widget

--- lib/webview_widget.dart
+++ lib/webview_widget.dart
@@ -1,10 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
 
-class webview_widget extends StatelessWidget {
-  const webview_widget({Key? key}) : super(key: key);
+class webview_widget extends StatefulWidget {
+  const webview_widget({Key? key, required this.url}) : super(key: key);
+
+  final url;
+
+  @override
+  _webview_widgetState createState() => _webview_widgetState();
+}
+
+class _webview_widgetState extends State<webview_widget> {
+  @override
+
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: CupertinoNavigationBar(),
+      body: Center(
+        child: WebView(
+          initialUrl: widget.url
+        )
+      )
+    );
   }
-}
